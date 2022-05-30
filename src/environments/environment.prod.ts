8097c7a6a732bc6f0ffa452244f43ed0266b780f
gaurav-all-1 <gauravsinghal5678gs678@gmail.com>
2022-05-30 16:04:00 +1200

    gaurav-change

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  demo: 'demo1',
+  SERVER_URL: 'http://18.219.65.148:8081',
+  MOLLA_URL: 'http://localhost:1337',
+  razorKey:"rzp_test_Lp2CeDDsYiDQLy"
+};
